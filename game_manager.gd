e0b6e1e1ed25e0b3b77166fe001fcdd017462b43
eilish-vp <eilish.paseos@gmail.com>
2025-03-10 22:30:53 +0900

    added function on_cell_pressed

--- game_manager.gd
+++ game_manager.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 
+# variables
+var board = [[null, null, null], [null, null, null], [null, null, null]] # 3x3 grid
+var currentPlayer = "X" # Start with X
+var gameOver = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,5 +16,19 @@ func _process(delta):
 	pass
 
 
-func on_cell_pressed():
-	pass # Replace with function body.
+func on_cell_pressed(cellIndex):
+	
+	if gameOver:
+		return
+	var row = (cellIndex / 3) # int division for row
+	var col = (cellIndex % 3) # modulo for column
+	if board[row][col] == null:
+		board[row][col] = currentPlayer
+	var button = get_child(1).get_child(cellIndex) # GridContainer is child 1
+	button.text = currentPlayer
+	button.disabled = true
+	check_win()
+	if not gameOver:
+		currentPlayer = "O" if currentPlayer == "X" else "X"
+		if currentPlayer == "O":
+			ai_move()
