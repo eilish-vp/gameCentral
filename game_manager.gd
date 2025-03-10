0d27fb4ef3fe308757d19bbc88bc273dffe55e91
eilish-vp <eilish.paseos@gmail.com>
2025-03-10 22:31:46 +0900

    function check_win

--- game_manager.gd
+++ game_manager.gd
@@ -32,3 +32,34 @@ func on_cell_pressed(cellIndex):
 		currentPlayer = "O" if currentPlayer == "X" else "X"
 		if currentPlayer == "O":
 			ai_move()
+
+
+func check_win():
+    
+	for i in range(3): # Check rows
+		if board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] != null:
+			end_game(board[i][0])
+			return
+    
+	for j in range(3): # Check columns
+		if board[0][j] == board[1][j] and board[1][j] == board[2][j] and board[0][j] != null:
+			end_game(board[0][j])
+			return
+    # Check diagonals
+	if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != null:
+		end_game(board[0][0])
+		return
+	if board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[0][2] != null:
+		end_game(board[0][2])
+		return
+	# Check draw
+	var is_draw = true
+	for i in range(3):
+		for j in range(3):
+			if board[i][j] == null:
+				is_draw = false
+				break
+		if not is_draw:
+			break
+	if is_draw:
+		end_game("Draw")
